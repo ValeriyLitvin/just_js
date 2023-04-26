ed4738de57053cb06c4bfe581b4e5a4d9ee7134b
ValeriyLitvin <valerkosssss@gmail.com>
2023-04-26 10:43:57 +0300

    йцла

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,23 @@
 // function getRandomHexColor() {
 //   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 // }
+// const user = {
+//   name: 'sam',
+//   age: 10,
+// };
+// console.log(user.name);
+// user.lastName = 'Litvin';
+// console.log(user.lastName);
+// const userCorrectName = user.name[0].toUpperCase() + user.name.slice(1);
+// // userCorrectName.toLowerCase();
+// console.log(userCorrectName);
+
+const numder = [-12, 3, 44, -156];
+const newNumber = [...numder];
+console.log(newNumber);
+console.log(numder);
+
+console.log(...numder);
+console.log(numder === newNumber);
+// const names = 'шукає';
+// console.log(names.toUpperCase().split(''));
